42d59a0b568d29ad3c4de41e40af23d7db63be09
Ingenieria-y-Dragones <andresjp0415@gmail.com>
2025-02-20 17:24:03 -0500

    Propiedades Aplicacion : Update

---
+++ src/main/java/co/edu/javeriana/caravana/init/DbInitializer.java
@@ -0,0 +1,5 @@
+package co.edu.javeriana.caravana.init;
+
+public class DbInitializer {
+    
+}
---
+++ src/main/java/co/edu/javeriana/caravana/repository/RutaRepository.java
@@ -0,0 +1,5 @@
+package co.edu.javeriana.caravana.repository;
+
+public class RutaRepository {
+    
+}
