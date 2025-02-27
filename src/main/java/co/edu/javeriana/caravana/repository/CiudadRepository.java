222bcfa166284e2e5cd79d7ee142b1d1affc1358
Ingenieria-y-Dragones <andresjp0415@gmail.com>
2025-02-27 17:10:55 -0500

    Actualización Repsitorios

--- src/main/java/co/edu/javeriana/caravana/repository/CiudadRepository.java
+++ src/main/java/co/edu/javeriana/caravana/repository/CiudadRepository.java
@@ -1,5 +1,9 @@
 package co.edu.javeriana.caravana.repository;
 
-public class CiudadRepository {
-    
+import co.edu.javeriana.caravana.model.Ciudad;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface CiudadRepository extends JpaRepository<Ciudad, Long> {
+
 }
