6248efc6d2abe05bac4ee36781049b18bdb151a0
Fer03Liz <fjlizarazog@gmail.com>
2025-04-18 16:39:31 -0500

    Version1 de home

--- frontend-caravana/src/app/pages/home/home.component.ts
+++ frontend-caravana/src/app/pages/home/home.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true, // Indica que es un componente independiente
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
