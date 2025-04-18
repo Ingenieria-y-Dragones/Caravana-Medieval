2e3ce97ab09bff5742eaded69947882e846db137
Fer03Liz <fjlizarazog@gmail.com>
2025-04-18 17:12:01 -0500

    Intento de conectar home a ciudad

--- frontend-caravana/src/app/app.routes.ts
+++ frontend-caravana/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { CiudadComponent } from './pages/ciudad/ciudad.component'; // 👈 Importa el componente de la ciudad
 
 export const routes: Routes = [
   { path: '', component: HomeComponent }, // 👈 Ruta principal vacía redirige al Home
-  // otras rutas 
+  { path: 'ciudad', component: CiudadComponent }, // 👈 Define la ruta para la ciudad
+  // otras rutas
 ];
