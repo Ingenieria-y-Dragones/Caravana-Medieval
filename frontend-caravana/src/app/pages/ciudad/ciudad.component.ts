e0e9ac38509172de3fbc3923eea70a53365b4641
Fer03Liz <fjlizarazog@gmail.com>
2025-04-19 10:35:35 -0500

    Navegacion entre home y ciudad correcta

--- frontend-caravana/src/app/pages/ciudad/ciudad.component.ts
+++ frontend-caravana/src/app/pages/ciudad/ciudad.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-ciudad',
+  standalone: true,
+  imports: [CommonModule], // 👈 Necesario para usar *ngFor y otras directivas estructurales
   templateUrl: './ciudad.component.html',
-  styleUrl: './ciudad.component.css'
+  styleUrls: ['./ciudad.component.css']
 })
 export class CiudadComponent {}
