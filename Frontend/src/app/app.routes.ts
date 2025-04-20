d8b604b8c62f16d63038f0fc2ac7d4843e10e8b4
Fer03Liz <fjlizarazog@gmail.com>
2025-04-19 19:29:44 -0500

    Se arreglo el menu home

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import {RutaListaComponent} from './ruta/ruta-lista/ruta-lista.component';
 import {RutaVistaComponent} from './ruta/ruta-vista/ruta-vista.component';
 import {RutaEditarComponent} from './ruta/ruta-editar/ruta-editar.component';
 import {CaravanaVistaComponent} from './caravana/caravana-vista/caravana-vista.component';
+import { MenuVistaComponent } from './menu/menu-vista/menu-vista.component';
 
 export const routes: Routes = [
+  {path: '', component: MenuVistaComponent},
   {path: 'ruta/lista/:id', component: RutaListaComponent}, // Equivalente en Spring Boot: /ruta/lista
   {path: 'ruta/vista/:id', component: RutaVistaComponent}, // Equivalente en Spring Boot: /ruta/vista/{id}
-  { path: 'ruta/editar/:id', component: RutaEditarComponent }, // Equivalente en Spring Boot: /ruta/editar/{id}
+  {path: 'ruta/editar/:id', component: RutaEditarComponent }, // Equivalente en Spring Boot: /ruta/editar/{id}
   {path: 'caravana/vista/:id', component: CaravanaVistaComponent}, // Equivalente en Spring Boot: /caravana/vista/{id}
   {path: '', pathMatch: 'full', redirectTo: 'ruta/lista'} // Equivalente en Spring Boot: /ruta/lista
 ];
